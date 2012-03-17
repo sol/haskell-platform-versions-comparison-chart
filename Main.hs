c35ed9a14917a645be66792f4f8f129132ba9d81
Simon Hengel <sol@typeful.net>
2012-03-17 15:27:00 +0100

    Add 2010.2.0.0

--- Main.hs
+++ Main.hs
@@ -174,3 +174,60 @@ hp_2011_2_0_0 = [
   , package "xhtml" "3000.2.0.1"
   , package "zlib" "0.5.3.1"
   ]
+
+hp_2010_2_0_0 = [
+    package "Cabal" "1.8.0.6"
+  , package "array" "0.3.0.1"
+  , package "base" "3.0.3.2"
+  , package "base" "4.2.0.2"
+  , package "bin-package-db" "0.0.0.0"
+  , package "bytestring" "0.9.1.7"
+  , package "containers" "0.3.0.0"
+  , package "directory" "1.0.1.1"
+  , Package "dph-base" "0.4.0" True
+  , Package "dph-par" "0.4.0" True
+  , Package "dph-prim-interface" "0.4.0" True
+  , Package "dph-prim-par" "0.4.0" True
+  , Package "dph-prim-seq" "0.4.0" True
+  , Package "dph-seq" "0.4.0" True
+  , package "extensible-exceptions" "0.1.1.1"
+  , package "ffi" "1.0"
+  , package "filepath" "1.1.0.4"
+  , Package "ghc" "6.12.3" True
+  , Package "ghc-binary" "0.5.0.2" True
+  , package "ghc-prim" "0.2.0.0"
+  , package "haskell98" "1.0.1.1"
+  , package "hpc" "0.5.0.5"
+  , package "integer-gmp" "0.2.0.1"
+  , package "old-locale" "1.0.0.2"
+  , package "old-time" "1.0.0.5"
+  , package "pretty" "1.0.1.1"
+  , package "process" "1.0.1.3"
+  , package "random" "1.0.0.2"
+  , package "rts" "1.0"
+  , package "syb" "0.1.0.2"
+  , package "template-haskell" "2.4.0.1"
+  , package "time" "1.1.4"
+  , package "unix" "2.4.0.2"
+  , package "GLUT" "2.1.2.1"
+  , package "HTTP" "4000.0.9"
+  , package "HUnit" "1.2.2.1"
+  , package "OpenGL" "2.2.3.0"
+  , package "QuickCheck" "2.1.1.1"
+  , package "cgi" "3001.1.7.3"
+  , package "deepseq" "1.1.0.0"
+  , package "fgl" "5.4.2.3"
+  , package "haskell-platform" "2010.2.0.0"
+  , package "haskell-src" "1.0.1.3"
+  , package "html" "1.0.1.2"
+  , package "mtl" "1.1.0.2"
+  , package "network" "2.2.1.7"
+  , package "parallel" "2.2.0.1"
+  , package "parsec" "2.1.0.1"
+  , package "regex-base" "0.93.2"
+  , package "regex-compat" "0.93.1"
+  , package "regex-posix" "0.94.2"
+  , package "stm" "2.1.2.1"
+  , package "xhtml" "3000.2.0.1"
+  , package "zlib" "0.5.2.0"
+  ]
