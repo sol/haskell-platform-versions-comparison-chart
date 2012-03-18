dd1d3395e5b96d20c0475c82f8eaeb48fc223469
Simon Hengel <sol@typeful.net>
2012-03-18 08:18:36 +0100

    Separate package lists by origin (ghc/platform)

--- Main.hs
+++ Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main (main) where
+module Main where
 import           Prelude hiding (div)
 import           Data.Foldable (forM_)
 
@@ -14,18 +14,21 @@ import           Text.Blaze.Html5.Attributes hiding (title, name)
 type PackageName = String
 type PackageVersion = String
 data Visibility = Exposed | Hidden
+  deriving (Eq, Show)
 
-data Package = Package
-  PackageName
-  PackageVersion
-  Visibility
+data Package = Package {
+  packageName       :: PackageName
+, packageVersion    :: PackageVersion
+, packageVisibility :: Visibility
+} deriving (Eq, Show)
 
 type PlatformVersion = String
 
-data Platform = Platform
-  PlatformVersion
-  [Package]
-
+data Platform = Platform {
+  platformVersion     :: PlatformVersion
+, platformGhcPackages :: [Package]
+, platformPackages    :: [Package]
+} deriving (Eq, Show)
 
 heading :: Html
 heading = "Haskell Platform Version Comparison Chart"
@@ -40,7 +43,7 @@ main = putStrLn . renderHtml . (docTypeHtml ! lang "en") $ do
   body . (div ! class_ "container" ! style "padding-top: 1em") $ do
     h1 heading
     blockquote . p $ do
-      em"Ever wanted to know what version of a package is in what Haskell Platform?"
+      em "Ever wanted to know what version of a package is in what Haskell Platform?"
       br
       em "Here you are!"
     table ! class_ "table table-bordered" $ do
@@ -63,12 +66,12 @@ main = putStrLn . renderHtml . (docTypeHtml ! lang "en") $ do
     packageIndex :: Map PackageName [(PlatformVersion, Package)]
     packageIndex = foldr f Map.empty releases
       where
-        f (Platform version xs) m = foldr g m xs
+        f (Platform version xs ys) m = foldr g m (xs ++ ys)
           where
             g x@(Package name _ _) = Map.insertWith' (++) name [(version, x)]
 
     versions :: [PlatformVersion]
-    versions = [v | Platform v _ <- releases]
+    versions = [v | Platform v _ _ <- releases]
 
 -- | Construct an exposed package.
 package :: PackageName -> PackageVersion -> Package
@@ -85,14 +88,14 @@ package name version = Package name version Exposed
 --
 releases :: [Platform]
 releases =
-  Platform "2011.4.0.0" hp_2011_4_0_0 :
-  Platform "2011.2.0.1" hp_2011_2_0_1 :
-  Platform "2011.2.0.0" hp_2011_2_0_0 :
-  Platform "2010.2.0.0" hp_2010_2_0_0 :
+  Platform "2011.4.0.0" ghc_7_0_4  hp_2011_4_0_0 :
+  Platform "2011.2.0.1" ghc_7_0_3  hp_2011_2_0_1 :
+  Platform "2011.2.0.0" ghc_7_0_2  hp_2011_2_0_0 :
+  Platform "2010.2.0.0" ghc_6_12_3 hp_2010_2_0_0 :
   []
 
-hp_2011_4_0_0 :: [Package]
-hp_2011_4_0_0 = [
+ghc_7_0_4 :: [Package]
+ghc_7_0_4 = [
     package "Cabal" "1.10.2.0"
   , package "array" "0.3.0.2"
   , package "base" "4.3.1.0"
@@ -119,7 +122,11 @@ hp_2011_4_0_0 = [
   , package "template-haskell" "2.5.0.0"
   , package "time" "1.2.0.3"
   , package "unix" "2.4.2.0"
-  , package "GLUT" "2.1.2.1"
+  ]
+
+hp_2011_4_0_0 :: [Package]
+hp_2011_4_0_0 = [
+    package "GLUT" "2.1.2.1"
   , package "HTTP" "4000.1.2"
   , package "HUnit" "1.2.4.2"
   , package "OpenGL" "2.2.3.0"
@@ -145,8 +152,8 @@ hp_2011_4_0_0 = [
   , package "zlib" "0.5.3.1"
   ]
 
-hp_2011_2_0_1 :: [Package]
-hp_2011_2_0_1 = [
+ghc_7_0_3 :: [Package]
+ghc_7_0_3 = [
     package "Cabal" "1.10.1.0"
   , package "array" "0.3.0.2"
   , package "base" "4.3.1.0"
@@ -173,7 +180,11 @@ hp_2011_2_0_1 = [
   , package "template-haskell" "2.5.0.0"
   , package "time" "1.2.0.3"
   , package "unix" "2.4.2.0"
-  , package "GLUT" "2.1.2.1"
+  ]
+
+hp_2011_2_0_1 :: [Package]
+hp_2011_2_0_1 = [
+    package "GLUT" "2.1.2.1"
   , package "HTTP" "4000.1.1"
   , package "HUnit" "1.2.2.3"
   , package "OpenGL" "2.2.3.0"
@@ -199,8 +210,8 @@ hp_2011_2_0_1 = [
   , package "zlib" "0.5.3.1"
   ]
 
-hp_2011_2_0_0 :: [Package]
-hp_2011_2_0_0 = [
+ghc_7_0_2 :: [Package]
+ghc_7_0_2 = [
     package "Cabal" "1.10.1.0"
   , package "array" "0.3.0.2"
   , package "base" "4.3.1.0"
@@ -227,7 +238,11 @@ hp_2011_2_0_0 = [
   , package "template-haskell" "2.5.0.0"
   , package "time" "1.2.0.3"
   , package "unix" "2.4.2.0"
-  , package "GLUT" "2.1.2.1"
+  ]
+
+hp_2011_2_0_0 :: [Package]
+hp_2011_2_0_0 = [
+    package "GLUT" "2.1.2.1"
   , package "HTTP" "4000.1.1"
   , package "HUnit" "1.2.2.3"
   , package "OpenGL" "2.2.3.0"
@@ -253,8 +268,8 @@ hp_2011_2_0_0 = [
   , package "zlib" "0.5.3.1"
   ]
 
-hp_2010_2_0_0 :: [Package]
-hp_2010_2_0_0 = [
+ghc_6_12_3 :: [Package]
+ghc_6_12_3 = [
     package "Cabal" "1.8.0.6"
   , package "array" "0.3.0.1"
   , package "base" "3.0.3.2"
@@ -288,7 +303,11 @@ hp_2010_2_0_0 = [
   , package "template-haskell" "2.4.0.1"
   , package "time" "1.1.4"
   , package "unix" "2.4.0.2"
-  , package "GLUT" "2.1.2.1"
+  ]
+
+hp_2010_2_0_0 :: [Package]
+hp_2010_2_0_0 = [
+    package "GLUT" "2.1.2.1"
   , package "HTTP" "4000.0.9"
   , package "HUnit" "1.2.2.1"
   , package "OpenGL" "2.2.3.0"
